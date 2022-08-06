d395a9cb3e46658795d1e017d0bdd7cae6a6d184
JTOLEDO <jessica-nadia@hotmail.com>
2022-08-06 20:15:38 -0300

    First Commit

---
+++ src/dio/PrimeiroPrograma.java
@@ -0,0 +1,22 @@
+package dio;
+
+import dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+		
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+	}
+
+}
+
+class libros {
+	private String nome;
+	private String npag;
+	
+}
